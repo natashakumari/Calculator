312b52676bed9ea3c69d503f0fd79815bba6385d
Natasha Kumari <nkumari@isimtl.local>
2020-07-09 16:31:54 -0400

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Natasha_Kumari_Final_Project
+{
+    class Calculator
+    {
+        public float Operand1 { get; set; }
+
+        public float Operand2 { get; set; }
+        public float Result { get; }
+
+        public Calculator(float op1,float op2)
+        {
+            Operand1 = op1;
+            Operand2 = op2;
+        }
+
+        public float Addition(float f1,float f2)
+        {
+            return f1 + f2;
+        }
+
+        public float Subtraction(float f1, float f2)
+        {
+            return f1 + f2;
+        }
+
+        public float Division(float f1, float f2)
+        {
+            return f1 + f2;
+        }
+
+        public float Multiplication(float f1, float f2)
+        {
+            return f1 + f2;
+        }
+
+        //public float Power(float f1, float f2)
+        //{
+
+        //    return f1;
+        //}
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Natasha_Kumari_Final_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+               ResultBlock.Text = (Convert.ToDouble(LeftText.Text) + Convert.ToDouble(RightText.Text)).ToString();
+        }
+
+        private void SubButton_Click(object sender, RoutedEventArgs e)
+        {
+                ResultBlock.Text = (Convert.ToDouble(LeftText.Text) - Convert.ToDouble(RightText.Text)).ToString();
+
+        }
+
+        private void MultButton_Click(object sender, RoutedEventArgs e)
+        {
+                ResultBlock.Text = (Convert.ToDouble(LeftText.Text) * Convert.ToDouble(RightText.Text)).ToString();
+
+        }
+
+        private void DivButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultBlock.Text = (Convert.ToDouble(LeftText.Text) / Convert.ToDouble(RightText.Text)).ToString();
+        }
+
+        private void PowButton_Click(object sender, RoutedEventArgs e)
+        {
+            int pow = Convert.ToInt32(RightText.Text);
+            double val = Convert.ToDouble(LeftText.Text);
+            double res = 0;
+
+            
+            for (int i = 0; i <= pow; i++)
+            {
+                res = val * i;
+            }
+            ResultBlock.Text = res.ToString();
+
+        }
+    }
+}
